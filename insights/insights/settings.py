dd691090ea4bbe1d67c34b5cabb9ccec1236ebb9
Andy McKay <andymckay@github.com>
2020-09-01 16:15:08 -0700

    changing debug breaks static

--- insights/insights/settings.py
+++ insights/insights/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["insights.ngrok.io", "127.0.0.1"]
+ALLOWED_HOSTS = ["insights.ngrok.io", '127.0.0.1']
 
 
 # Application definition

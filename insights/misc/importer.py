519f3676cb4a217567b439453e00806c69f8befa
Andy McKay <andymckay@github.com>
2020-09-02 09:48:40 -0700

    move to recurse

--- insights/misc/importer.py
+++ insights/misc/importer.py
@@ -7,11 +7,98 @@ from github import Github
 
 from misc.models import Artifact, Repo, Run, Token, Workflow
 
+class Log():
+    def __init__(self):
+        self.data = []
+
+    def append(self, string):
+        string = "%s: %s" % (datetime.datetime.now(), string)
+        print(string)
+        self.data.append(string)
+
+
+def get_workflows(log, repo, repo_from_api, k):
+    workflows = repo_from_api.get_workflows().get_page(k)
+    if not workflows:
+        log.append("No workflows for page: %s, stopping" % k)
+        return
+
+    log.append("Found workflows in page: %s" % k)
+    for workflow_from_api in workflows:
+        workflow = Workflow.objects.get_or_create(
+            workflow_id=workflow_from_api.id, repo=repo
+        )[0]
+        workflow.name = workflow_from_api.name
+        workflow.path = workflow_from_api.path.split("/")[-1]
+        workflow.save()
+        log.append("Saved workflow: %s" % workflow.path)
+
+    k += 1
+    get_workflows(log, repo, repo_from_api, k)
+
+def get_runs(log, workflow, workflow_from_api, k):
+    runs = workflow_from_api.get_runs().get_page(k)
+    if not runs:
+        log.append("No runs for page: %s, stopping" % k)
+        return
+
+    log.append("Found runs in page: %s" % k)
+    for run_from_api in runs:
+        if run_from_api.status == "completed":
+            run = Run.objects.get_or_create(
+                run_id=run_from_api.id, workflow_id=workflow.id
+            )[0]
+            run.conclusion = run_from_api.conclusion
+            run.start_time = run_from_api.created_at
+            run.end_time = run_from_api.updated_at
+            run.elapsed = run_from_api.updated_at - run_from_api.created_at
+            run.status = run_from_api.status
+            run.save()
+            log.append("Saved run: %s" % run.id)
+
+    k += 1
+    get_runs(log, workflow, workflow_from_api, k)
+
+def get_artifacts(log, repo, run, access, k):
+    res = requests.get(
+        "https://api.github.com/repos/%s/actions/runs/%s/artifacts?page=%s"
+        % (repo.nwo, run.run_id, k),
+        headers={
+            "Authorization": "token %s" % access,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    )
+    res.raise_for_status()
+    data = res.json()
+    if not data["artifacts"]:
+        log.append("No artifacts found for page: %s, stopping" % k)
+        return
+
+    log.append("Found artifacts in page: %s" % k)
+    for artifact_from_api in data["artifacts"]:
+        artifact = Artifact.objects.get_or_create(
+            artifact_id=artifact_from_api["id"], run=run
+        )[0]
+        artifact.created_at = datetime.datetime.strptime(
+            artifact_from_api["created_at"], "%Y-%m-%dT%H:%M:%SZ"
+        )
+        artifact.expired = artifact_from_api["expired"]
+        artifact.size_in_bytes = artifact_from_api["size_in_bytes"]
+        artifact.name = artifact_from_api["name"]
+
+        artifact.download = artifact_from_api["archive_download_url"]
+        artifact.save()
+        log.append("Artifact saved %s" % artifact.id)
+
+    k += 1
+    get_artifacts(log, repo, run, access, k)
 
 @login_required
 def import_repo(request, pk):
+    log = Log()
+    log.append("Starting run.")
     access = Token.objects.all()[0].access
-    log = []
     log.append("Getting access.")
     g = Github(access)
 
@@ -21,56 +108,15 @@ def import_repo(request, pk):
     repo.public = not repo_from_api.private
     repo.save()
 
-    for workflow_from_api in repo_from_api.get_workflows().get_page(0):
-        workflow = Workflow.objects.get_or_create(
-            workflow_id=workflow_from_api.id, repo=repo
-        )[0]
-        workflow.name = workflow_from_api.name
-        workflow.path = workflow_from_api.path.split("/")[-1]
-        workflow.save()
-        log.append("Saved workflow: %s" % workflow.path)
+    get_workflows(log, repo, repo_from_api, 0)
 
     for workflow in Workflow.objects.filter(repo=repo):
         workflow_from_api = repo_from_api.get_workflow(str(workflow.workflow_id))
-        for run_from_api in workflow_from_api.get_runs():
-            if run_from_api.status == "completed":
-                run = Run.objects.get_or_create(
-                    run_id=run_from_api.id, workflow_id=workflow.id
-                )[0]
-                run.conclusion = run_from_api.conclusion
-                run.start_time = run_from_api.created_at
-                run.end_time = run_from_api.updated_at
-                run.elapsed = run_from_api.updated_at - run_from_api.created_at
-                run.status = run_from_api.status
-                run.save()
-                log.append("Saved run: %s" % run.id)
-
+        get_runs(log, workflow, workflow_from_api, 0)
         for run in Run.objects.filter(workflow_id=workflow.id):
-            res = requests.get(
-                "https://api.github.com/repos/%s/actions/runs/%s/artifacts"
-                % (repo.nwo, run.run_id),
-                headers={
-                    "Authorization": "token %s" % access,
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                },
-            )
-            res.raise_for_status()
-            data = res.json()
-            for artifact_from_api in data["artifacts"]:
-                artifact = Artifact.objects.get_or_create(
-                    artifact_id=artifact_from_api["id"], run=run
-                )[0]
-                artifact.created_at = datetime.datetime.strptime(
-                    artifact_from_api["created_at"], "%Y-%m-%dT%H:%M:%SZ"
-                )
-                artifact.expired = artifact_from_api["expired"]
-                artifact.size_in_bytes = artifact_from_api["size_in_bytes"]
-                artifact.name = artifact_from_api["name"]
+            # PyGithub uses Python syntax, first page is 0.
+            # Looks like the REST API starts with the first page at 1.
+            get_artifacts(log, repo, run, access, 1)
 
-                artifact.download = artifact_from_api["archive_download_url"]
-                artifact.save()
-                log.append("Artifact saved %s" % artifact.id)
-
-    context = {"log": log, "repo": repo}
+    context = {"log": log.data, "repo": repo}
     return render(request, "misc/import-repo.html", context)
